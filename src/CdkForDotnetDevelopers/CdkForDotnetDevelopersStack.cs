bfa2451c748e21f36d34360f98f8cb3ebf13f83a
Mukesh Murugan <iammukeshm@gmail.com>
2023-05-28 18:53:13 +0530

    Initial commit

---
+++ src/CdkForDotnetDevelopers/CdkForDotnetDevelopersStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkForDotnetDevelopers
+{
+    public class CdkForDotnetDevelopersStack : Stack
+    {
+        internal CdkForDotnetDevelopersStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
